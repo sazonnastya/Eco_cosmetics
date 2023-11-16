e01490b7f05fafa57ffc8e68b02e42566a7fd7ca
Anastasia <sazonnastya7@gmail.com>
2023-11-16 15:40:47 +0100

    eco-cosmetic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-'use strict';
+// Береш кнопочки по класам з DOM:
+const closeButton = document.querySelector('.menu__close-btn');
+const openButton = document.querySelector('.top-bar__menu');
+
+// Отримуєш меню:
+const nav = document.querySelector('.menu');
+
+// Якщо на кнопочку клікнули - додати клас
+closeButton.addEventListener('click', () => {
+  nav.classList.add('menu-close');
+});
+
+// Якщо на кнопочку клікнули - видалити клас
+openButton.addEventListener('click', () => {
+  nav.classList.remove('menu-close');
+});
